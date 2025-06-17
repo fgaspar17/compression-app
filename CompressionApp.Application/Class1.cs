ef4e9d6627f6d21fba21e66a1c348a316f28a18d
Fran Gaspar <nectryk@gmail.com>
2025-06-17 18:06:15 +0200

    compress function added for gzip

--- CompressionApp.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CompressionApp.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ CompressionApp.Application/GzipCompressionService.cs
@@ -0,0 +1,13 @@
+﻿using System.IO.Compression;
+
+namespace CompressionApp.Application;
+
+public class GzipCompressionService
+{
+    public async Task CompressFileAsync(Stream inputStream, string filePathDestination)
+    {
+        using FileStream destinationStream = new (filePathDestination, FileMode.CreateNew);
+        using GZipStream compressionStream = new(destinationStream, CompressionLevel.Optimal);
+        await inputStream.CopyToAsync(compressionStream);
+    }
+}
